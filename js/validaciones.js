1fcf49417d488f12174f8dc4c157170e7ce5275f
Katherine <kat.alemanv17@gmail.com>
2023-09-25 20:10:01 -0600

    validacion de fechas y creacion de funcion validar

---
+++ js/validaciones.js
@@ -0,0 +1,32 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+}
+
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+};
+
+inputNacimiento.addEventListener("blur", (evento) => {
+    validarNacimiento(evento.target);
+})
+
+function validarNacimiento(input) {
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+
+    if(!mayorDeEdad(fechaCliente)) {
+        mensaje = "Debes tener al menos 18 años para completar el registro."
+    }
+
+    input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(fecha.getUTCFullYear() + 18, fecha.getUTCMonth(), fecha.getUTCDate());
+
+    return diferenciaFechas <= fechaActual;
+}
